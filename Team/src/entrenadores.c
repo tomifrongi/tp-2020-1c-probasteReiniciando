55b445673d9c60a09fcdf44e279dcd70667ef85e
ignacio <ignaciofmunoz@github.com>
2020-05-08 07:42:28 -0300

    armo archivo de config

--- Team/src/entrenadores.c
+++ Team/src/entrenadores.c
@@ -2,4 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include "team.h"
+#include<commons/log.h>
 
+int cargar_objetivos(t_entrenador*){
+
+	t_config *config=t_config *config_create("a.config");
+
+}
