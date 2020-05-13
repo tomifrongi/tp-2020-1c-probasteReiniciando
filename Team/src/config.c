d3bea9d9e5c04d0ac987610fae94bb729fe31ef1
utnso <utnso@ubuntu-server>
2020-05-13 16:27:08 -0300

    corrijo errores de tipo

--- Team/src/config.c
+++ Team/src/config.c
@@ -1,11 +1,30 @@
 #include<commons/config.h>
 #include<commons/collections/list.h>
 #include<commons/collections/queue.h>
-
+#include <stdio.h>
 #include <stdlib.h>
 #include<commons/string.h>
 #include "config.h"
 #include "entrenadores.h"
+
+void separar_valores(char*string_valores, t_queue * cola_valores) { //divide los valores de un arreglo del config
+	int long_string = string_length(string_valores);
+	int i = 0;
+	char*str = "";
+	char c;
+	while (i != long_string) {
+
+		while (c != '|') {
+			c = string_valores[i];
+			string_append(str, c); //a la palabra currente le sigo metiendo letras// ver que lo haga bien
+			string_valores++;
+		}
+		queue_push(cola_valores, str);
+		str = "";
+
+		i++;
+	}
+}
 
 void cargar_objetivos(t_entrenador* entrenador) {
 	t_config * config = config_create("a.config");
@@ -18,10 +37,10 @@ void cargar_objetivos(t_entrenador* entrenador) {
 			"OBJETIVOS_ENTRENADORES");
 	//t_list * list_valores = list_create();
 	t_queue * valores_pokemones =queue_create();
-	separar_valores(&array_objetivos,list_valores);
+	separar_valores(&array_objetivos,valores_pokemones);
 	while (queue_is_empty(valores_pokemones)==0){
 		entrenador->pokemonesBuscados->especie = queue_pop(valores_pokemones);
-		entrenador->pokemonesBuscados=entrenador->pokemonesBuscados->sgte ;
+		entrenador->pokemonesBuscados=entrenador->pokemonesBuscados->sgte;
 	}
 	config_destroy(config);
 
@@ -29,21 +48,4 @@ void cargar_objetivos(t_entrenador* entrenador) {
 int separar_array(char**array) {
 	return 0;
 }
-void separar_valores(char*string_valores, t_list * list_valores) { //divide los valores de un arreglo del config
-	int long_string = string_length(string_valores);
-	int i = 0;
-	char*str = "";
-	char c;
-	while (i != long_string) {
 
-		while (c != '|') {
-			c = string_valores[i];
-			string_append(str, c); //a la palabra currente le sigo metiendo letras// ver que lo haga bien
-			string_valores++;
-		}
-		list_add(list_valores, str);
-		str = "";
-
-		i++;
-	}
-}
