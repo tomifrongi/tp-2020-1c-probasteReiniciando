d2da97faf2c5f2e342ec050319ac1e5b78d76c9f
Frongi <tomifrongi@gmail.com>
2020-07-18 14:52:41 -0300

    Limpieza

--- Gameboy/src/Gameboy.c
+++ Gameboy/src/Gameboy.c
@@ -1,6 +1,8 @@
 #include "Gameboy.h"
 
 //TODO revisar que esten todos y bien todos los logs
+//TODO envio del ACK al broker
+//TODO caso de error de conexion con alguno de los modulos.
 
  int main(void) {
 
