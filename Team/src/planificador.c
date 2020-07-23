7d62e17c9ae205bcc1cc9b153542b241e8a511e1
ignacio muñoz <Ignacio40309782@yahoo.com>
2020-07-23 08:22:25 -0300

    Update planificador.c

--- Team/src/planificador.c
+++ Team/src/planificador.c
@@ -1,4 +1,3 @@
-#include "team.h"
 #include "planificador.h"
 
 #include <stdbool.h>
@@ -16,7 +15,7 @@ void* crear_hilos_entrenadores( team) { //debe hacerse al inciar el proceso team
 	for (int i = 0; i < cant_entrenadores; i++) {
 
 		phtread_create(hilos[i], NULL, planificar, NULL);
-		pthread_join(hilo[i]);
+		pthread_join(hilos[i]);
 
 	}
 	return &hilos;
@@ -24,126 +23,198 @@ void* crear_hilos_entrenadores( team) { //debe hacerse al inciar el proceso team
 
 // TODO
 //planificar team
-activar_proceso_reconexion();//un thread
-abrir_socket_gameboy();//marcos
-recibir_mensajes_gameboy(socket_gameboy);//marcos
-team_inicializar(team);
+activar_proceso_reconexion(); //un thread
+abrir_socket_gameboy(); //marcos
+recibir_mensajes_gameboy( socket_gameboy); //marcos
+team_inicializar( team);
 crear_pokemon();
-planificar_entrenador(team,pokemon);
-verificar_deadlock(team);
-salvar_deadlocks(team);
+planificar_entrenador( team, pokemon);
+verificar_deadlock( team);
+salvar_deadlocks( team);
 //suscribirse a colas
-suscribirse_apparead();//marcos
-suscribirse_caught();//marcos
-suscribirse_localized();//marcos
+suscribirse_apparead(); //marcos
+suscribirse_caught(); //marcos
+suscribirse_localized(); //marcos
 //pedir_pokemones_necesariosg
-team_especies_pokemones_necesarios(team);
-connectar_con_broker();//marcos
-enviar_mensaje_a_broker(get, list_get(especies_necesarias, i), socket);//marcos
-cerrar_coneccion_broker();//marcos
+team_especies_pokemones_necesarios( team);
+connectar_con_broker(); //marcos
+enviar_mensaje_a_broker( get, list_get( especies_necesarias, i), socket); //marcos
+cerrar_coneccion_broker(); //marcos
 //verificar_nuevo_localized
-hay_nuevos_mensajes();//marcos
-recibir_mensaje();//marcos   //aca la idea es que devuelva por parametro el pokemon para despues capturarlo
-team_necesita_especie(esṕecie) ;
-primer_mensaje(team,especie);//solo me quedo con el primer localized de cada especie ,no puede haber dos en mapa sueltos de cada especie
-agregar_nuevos(team);
-no_es_primer_mensaje(team,especie);//avisa que ya no es el primer mensaje el proximo que vendra de esa especie,tendra que volverlo a poner en blanco el entrenador al capturar,no se si n se podria hacer directamente contando los que hay sueltos en mapa
-descartar_mensaje(mensaje);
+hay_nuevos_mensajes(); //marcos
+recibir_mensaje(); //marcos   //aca la idea es que devuelva por parametro el pokemon para despues capturarlo
+team_necesita_especie( esṕecie);
+primer_mensaje( team, especie); //solo me quedo con el primer localized de cada especie ,no puede haber dos en mapa sueltos de cada especie
+agregar_nuevos( team);
+no_es_primer_mensaje( team, especie); //avisa que ya no es el primer mensaje el proximo que vendra de esa especie,tendra que volverlo a poner en blanco el entrenador al capturar,no se si n se podria hacer directamente contando los que hay sueltos en mapa
+descartar_mensaje( mensaje);
 //__________________________Guion_____________________________________________________________________-
-void planificar_team(t_team*team) {
-	bool suscripcion_ok=suscribirse_a_colas(team);
-	if (suscripcion_ok==false){
-		activar_proceso_reconexion();//un thread
-		int socket_gameboy=abrir_socket_gameboy();//no se si no se hace aun cuando el broker si conecta
-		recibir_mensajes_gameboy(socket_gameboy);//otro thread
-	}
-	team_inicializar(team);//para las cuestiones de objetivo y demas carga inicial
-	pedir_pokemones_necesarios(team); //gets
-	t_pokemon*a_capturar=crear_pokemon();//crear pokemon en blanco ,mmalloc y demas(faltaria para despues una de liberar)
 
-	while(team_cumplio_objetivo_global(team)==false) {
-{
-		if(verificar_nuevo_localized(team,pokemon)==true&&team_puede_capturar(team)==true)//las repuestas a los get,pueden ser dirigidos a otro team
-		{
-			/*
-			 *
-			 * la idea en este punto es mandarlo a capturar,  osea ponerlo en ready, despues el algoritmo de planificacion dira cuando se ejecuta
-		*/
-			planificar_entrenador(team,pokemon);}
-		else{//mientras no llegan mensajes o se quedo trabajdo el team ,se purgan los deadlocks
-			if (verificar_deadlock(team)){//si la causa es un deadlock aca se soluciona,si es que no esta llegando mensajes se esperaran
-				salvar_deadlocks(team);
-			}
-			}
-		}
-}
-
-}
 bool suscribirse_a_colas(t_team*team) {
 	int socket_apparead = suscribirse_apparead();
 	int socket_caught = suscribirse_caught();
 	int socket_localized = suscribirse_localized();
-return((socket_localized ==0)||(socket_caught==0)||(socket_apparead==0)) ;
+	return ((socket_localized == 0) || (socket_caught == 0) || (socket_apparead == 0));
 
+	pedir_pokemones_necesarios(t_team * team);
+	{
+		t_list*especies_necesarias = list_create();
+		especies_necesarias = team_especies_pokemones_necesarios(team); //solo mando los tipos de especie que me interesan,no la cantidad que necesite
 
+		for (int i = 0; i < list_size(especies_necesarias); i++) {
+			int socket = connectar_con_broker(); //tema sockets
+			enviar_mensaje_a_broker(get, list_get(especies_necesarias, i), socket);
+			//creo que son las tres cosas que necesita nro de socket,especie
+			cerrar_coneccion_broker();
+		}
+	}
 
-pedir_pokemones_necesarios(t_team*team) {
-t_list*especies_necesarias = list_create();
-especies_necesarias = team_especies_pokemones_necesarios(team); //solo mando los tipos de especie que me interesan,no la cantidad que necesite
+	bool verificar_nuevo_localized(t_team*team, t_pokemon*pokemon) { //TODO
+		if (hay_nuevos_mensajes()) {
+			t_pokemon*pokemon = recibir_mensaje(); //aca la idea es que devuelva por parametro el pokemon para despues capturarlo
+			if (team_necesita_especie(esṕecie) & primer_mensaje(team, especie)) { //NO ME GUSTA ASI...//solo me quedo con el primer localized de cada especie ,no puede haber dos en mapa sueltos de cada especie
+				agregar_nuevos(team); //agrego a la lista de pokemones sueltos los que llegaron //mapeo
+				no_es_primer_mensaje(team, especie); //avisa que ya no es el primer mensaje el proximo que vendra de esa especie,tendra que volverlo a poner en blanco el entrenador al capturar,no se si n se podria hacer directamente contando los que hay sueltos en mapa
 
-for (int i = 0; i < list_size(especies_necesarias); i++) {
-	int socket = connectar_con_broker(); //tema sockets
-	enviar_mensaje_a_broker(get, list_get(especies_necesarias, i), socket);
-	//creo que son las tres cosas que necesita nro de socket,especie
-	cerrar_coneccion_broker();
+			} else {
+				descartar_mensaje(mensaje);
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/* entrenadores en new
+	 *
+	 * envio n mensajes get de los pokes buscados
+	 *
+	 *
+	 * llegan n  mensajes localized ,
+	 *     descarto los que no sirven
+	 *     me quedo con el primer mensaje del localized de cada esoecie(el segundo lo tengo guardado pro si acaso)
+	 *     mapeo el primer poke de localized de la especie
+	 *
+	 planificar_entrenador:* planificao(paso a ready) al entrenador mas cercano que no este haceiendo nada(que este en new , o block por que no teine nada para hacer)
+	 * si no hay nadie en exec,paso al entrenador que esta en ready a exec
+	 * se ejecutan los ciclos de cpu para mover al entrenador en el mapa
+	 * al llegar al pokemon se realiza un nuevo connect al brocker y se envia el mensaje "catch squitrle 4 4",
+	 *   tomamos el id que nos deuvelve broker yse cierra conexion ,se cierra socket y se bloquea el entrenador
+	 *  recibimos el caught afirmando/negando la atrapada con el id correspondiente y si es positivo le sumamos el pokemon al entrenador
+	 *       (*1)hago verificacion de objetivos: si colmo capacidad maxima de captura :queda bloqueado a la espera de deadlock,si cumplio sus objetivos va a exit
+	 *        si quedo algun pokemon suelto en mapa por que llegaron mas rapido que lo que los entrenadores capturan,lo pongo en estado ready y lo vuelvo a planificar
+	 *        si no paso nada de lo anterior (que haya pokemones sueltos o que cumplio sus objetivos) queda bloqueado a la espera de que aparezcan pokemones
+	 *
+	 /*
+	 *otro caso:localized envio 2 pokemones pero el obj global solo necesita 1 ,planifico al mas cercano,pero no borro el otro por si el caught es negativo
+	 *otro caos: localized envio 2 pokemones  y necesito los 2,planifico a dos pokemones mas cercanos para ir a capturar
+	 *
+	 *deteccion de deadlock:
+	 *   el entrenador debe estar bloqeueado
+	 *   deben poseer pokemones que  no necesitan
+	 *   se forma espera circular entre entrenadoreses
+	 *
+	 * intercambio:se mueve un entrenador al lugar del otro
+	 * se realiza el intercambio que consume  5 ciclos
+	 * se realizan las mismas verificaciones que en el caso de caught (*1)
+	 */
+
+//armo los planificadores:
+//atrapada:entrenador y pokemon involucrado
+	int get_indice(t_team*team, hilo_entrenador) {
+		int indice = 0;
+		if (list_size(team->entrenadores_planificados) == 0) { //si no hay elementos lo pongo primero
+			return 0;
+		}
+		while (indice < list_size(team->entrenadores_planificados)) {
+			t_entrenador*hilo_otro_entrenador = list_get(team->entrenadores_planificados, indice);
+			if (hilo_entrenador->tiempo_rafaga > hilo_otro_entrenador->tiempo_rafaga) {
+				indice++;
+			} else {
+				return indice;
+			}
+
+		}
+
+	}
+
 }
+planificar_entrenador(t_team * team, t_pokemon * pokemon)
+//pasar su hilo a ready
+{
+
+	t_entrenador*entrenador = entrenador_mas_cercano(team->entrenadores, pokemon)
+//aca armo el hilo y se los paso a los planificadores//seria otro tad...
+	switch (team->planificador) {
+	case FIFO:
+	case RR:
+
+		list_add(team->entrenadores_planificados, hilo_entrenador); //se agrega al final
+		break;
+	case SJF_CD:
+		int indice = get_indice(team, hilo_entrenador);
+		if (indice == 0) {
+			desalojar_hilo(); //TODO que desaloje y se ejecute el nuevo hilo
+		}
+		list_add_in_index(team->entrenadores_planificados, indice, hilo_entrenador);
+
+		break;
+	case SJF_SD: //lo agrega ordenado segun su tiempo
+
+		list_add_in_index(team->entrenadores_planificados, get_indice(team, hilo_entrenador), hilo_entrenador);
+		break;
+	}
+
 }
 
+int tiempo_rafaga( entrenador, pokemon) {
+	return team->retardo_ciclo_cpu * distancia_entrenador_pokemon(entrenador, pokemon);
 
-bool verificar_nuevo_localized(team,pokemon) {//TODO
-if (hay_nuevos_mensajes()) {
-	t_pokemon*pokemon=recibir_mensaje();//aca la idea es que devuelva por parametro el pokemon para despues capturarlo
-	if(team_necesita_especie(esṕecie) & primer_mensaje(team,especie)){//NO ME GUSTA ASI...//solo me quedo con el primer localized de cada especie ,no puede haber dos en mapa sueltos de cada especie
-	agregar_nuevos(team);//agrego a la lista de pokemones sueltos los que llegaron //mapeo
-	no_es_primer_mensaje(team,especie);//avisa que ya no es el primer mensaje el proximo que vendra de esa especie,tendra que volverlo a poner en blanco el entrenador al capturar,no se si n se podria hacer directamente contando los que hay sueltos en mapa
-
-	}else{descartar_mensaje(mensaje);
-	return false;}
-}
-return true;
 }
 
-/* entrenadores en new
- *
- * envio n mensajes get de los pokes buscados
- *
- *
- * llegan n  mensajes localized ,
- *     descarto los que no sirven
- *     me quedo con el primer mensaje del localized de cada esoecie(el segundo lo tengo guardado pro si acaso)
- *     mapeo el primer poke de localized de la especie
- *
- planificar_entrenador:* planificao(paso a ready) al entrenador mas cercano que no este haceiendo nada(que este en new , o block por que no teine nada para hacer)
- * si no hay nadie en exec,paso al entrenador que esta en ready a exec
- * se ejecutan los ciclos de cpu para mover al entrenador en el mapa
- * al llegar al pokemon se realiza un nuevo connect al brocker y se envia el mensaje "catch squitrle 4 4",
- *   tomamos el id que nos deuvelve broker yse cierra conexion ,se cierra socket y se bloquea el entrenador
- *  recibimos el caught afirmando/negando la atrapada con el id correspondiente y si es positivo le sumamos el pokemon al entrenador
- *       (*1)hago verificacion de objetivos: si colmo capacidad maxima de captura :queda bloqueado a la espera de deadlock,si cumplio sus objetivos va a exit
- *        si quedo algun pokemon suelto en mapa por que llegaron mas rapido que lo que los entrenadores capturan,lo pongo en estado ready y lo vuelvo a planificar
- *        si no paso nada de lo anterior (que haya pokemones sueltos o que cumplio sus objetivos) queda bloqueado a la espera de que aparezcan pokemones
- *
- /*
- *otro caso:localized envio 2 pokemones pero el obj global solo necesita 1 ,planifico al mas cercano,pero no borro el otro por si el caught es negativo
- *otro caos: localized envio 2 pokemones  y necesito los 2,planifico a dos pokemones mas cercanos para ir a capturar
- *
- *deteccion de deadlock:
- *   el entrenador debe estar bloqeueado
- *   deben poseer pokemones que  no necesitan
- *   se forma espera circular entre entrenadoreses
- *
- * intercambio:se mueve un entrenador al lugar del otro
- * se realiza el intercambio que consume  5 ciclos
- * se realizan las mismas verificaciones que en el caso de caught (*1)
- */
+void planificacion_rr( team, entrenador_cercano) {
+	list_add(team->entrenadores_planificados, entrenador_cercano);
+	int tiempo_total_captura = tiempo_rafaga(entrenador, pokemon); //lo paso a una variable por que sino al moverse el entrenador altera la cuenta
+	for (i = 0; i <= quantum; i++) {
+		if (i == quantum) { //agoto tiempo de quuantum,pasa a bloqueado
+			i++;
+		}
+	}
+}
+planificacion_sjf_cd( team, hilo_entrenador)
+
+//----------------------------------------------------------GUION/funcion principal:
+void planificar_team(t_team*team) {
+	//bool suscripcion_ok = suscribirse_a_colas(team);
+	if (suscribirse_a_colas(team) == false) {
+		activar_proceso_reconexion(); //un thread
+		int socket_gameboy = abrir_socket_gameboy(); //no se si no se hace aun cuando el broker si conecta
+		recibir_mensajes_gameboy(socket_gameboy); //otro thread
+	}
+	pedir_pokemones_necesarios(team); //gets
+	t_pokemon*pokemon_a_capturar = malloc(sizeof(t_pokemon)); //crear pokemon en blanco ,mmalloc y demas(faltaria para despues una de liberar)
+
+	while (team_cumplio_objetivo_global(team) == false) { //acomodar esto
+		{
+			if (verificar_nuevo_localized(team,pokemon_a_capturar) == true && team_puede_capturar(team) == true) //las repuestas a los get,pueden ser dirigidos a otro team
+			{
+				/*
+				 *
+				 * la idea en este punto es mandarlo a capturar,  osea ponerlo en ready, despues el algoritmo de planificacion dira cuando se ejecuta
+				 */
+				planificar_entrenador(team, pokemon); //aca ya tendria la cola de hilos lista y actualizada para que laburen
+
+			} else { ///mientras no llegan mensajes o se quedo trabajdo el team ,se purgan los deadlocks
+				if (verificar_deadlock(team)) { //si la causa es un deadlock aca se soluciona,si es que no esta llegando mensajes se esperaran
+					team->cantidad_deadlocks_detectados++;
+					salvar_deadlocks(team);
+				}
+				else{//en este punto se supone que no hay pokemones libres en mapa ni deadlock, hay que seguir el curso normal
+					ejecutar_planificador();
+				}
+			}
+		}
+	} //de aca en adelante se cumplio el objetivo del team...van las funciones de cerrar
+
+}
+
 
